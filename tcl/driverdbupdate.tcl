14df3598d6ab20312a7f59ea78139c51589be7b2
gregzakh <grishanz@yandex.ru>
2019-06-23 01:19:07 +0500

    tcl: driverdbupdate.tcl (minor fixes)

--- tcl/driverdbupdate.tcl
+++ tcl/driverdbupdate.tcl
@@ -8,11 +8,15 @@ set key   {HKEY_LOCAL_MACHINE\SYSTEM\DriverDatabase}
 set epoch 116444736000000000 ; # epoch as FILETIME
 set nano  10000000           ; # hundred nanoseconds
 
+proc uint_t {val} {
+  return [expr $val < 0 ? $val & 0xFFFFFFFF : $val]
+}
+
 if {[catch {binary scan [registry get $key updatedate] ii l h} e]} {
   puts stderr [concat "\[Error\]: " [lindex [split $e :] 0]]
   exit 1
 }
 
 puts [clock format [expr ( \
-  ($h << 32 | $l) - $epoch \
+  ([uint_t $h] << 32 | [uint_t $l]) - $epoch \
 ) / $nano] -format {%m-%d-%Y %H:%M:%S}]
