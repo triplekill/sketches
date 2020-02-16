83e0351692623a930f56bd95303531cdf8c6ff7d
gregzakh <grishanz@yandex.ru>
2020-02-16 18:43:39 +0500

    c: sysroot.c

---
+++ c/sysroot.c
@@ -0,0 +1,34 @@
+/*
+#ifndef UNICODE
+  #define UNICODE
+#endif
+
+#include <stdio.h>
+
+int wmain(void) {
+  wprintf(L"System root: %s\n", (wchar_t *)0x7FFE0030);
+
+  return 0;
+}
+*/
+#ifndef UNICODE
+  #define UNICODE
+#endif
+
+#include <windows.h>
+#include <stdio.h>
+
+#pragma comment(lib, "ntdll")
+
+NTSYSAPI
+PWSTR
+NTAPI
+RtlGetNtSystemRoot(
+   VOID
+);
+
+int wmain(void) {
+  wprintf(L"System root: %s\n", RtlGetNtSystemRoot());
+
+  return 0;
+}
