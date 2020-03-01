54a603aa00a59a96ed6089b7f6a621203a4c7eff
gregzakh <grishanz@yandex.ru>
2020-03-01 17:43:46 +0500

    tcl: prefetcher.tcl

---
+++ tcl/prefetcher.tcl
@@ -0,0 +1,27 @@
+if {$tcl_platform(os) ne "Windows NT"} {
+  throw {OSERROR} {Wrong operating system.}
+}
+
+package require registry
+
+set HKLM {HKEY_LOCAL_MACHINE}
+set key  {SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management\PrefetchParameters}
+
+set status [
+   dict create 0 {disabled} \
+               1 {enabled for applications} \
+               2 {enabled for boot only} \
+               3 {enabled for boot and applications} \
+]
+
+proc getstatus {value item} {
+  global HKLM key status
+  if {[catch {set tmp [registry get "$HKLM\\$key" $value]} e]} {
+    puts stderr [concat "\[Error\]:" [lindex [split $e :] 0]]
+    exit 1
+  }
+  puts "$item is [dict get $status $tmp]."
+}
+
+getstatus EnablePrefetcher Prefetcher
+getstatus EnableSuperfetch Superfetch
