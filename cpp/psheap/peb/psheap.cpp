f80bb07abd89a5210f37b6e60b917ed1f65f83d4
gregzakh <grishanz@yandex.ru>
2020-03-15 00:21:40 +0500

    cpp: psheap/peb (extended)

--- cpp/psheap/peb/psheap.cpp
+++ cpp/psheap/peb/psheap.cpp
@@ -1,6 +1,7 @@
 #include "psheap.hpp"
 
 #include <iostream>
+#include <iomanip>
 #include <string>
 #include <vector>
 #include <memory>
@@ -33,7 +34,7 @@ int wmain(int argc, wchar_t *argv[]) {
 
   if (!LocateSignatures()) {
     PrintErrMessage(0L);
-    exit(1);
+    return 1;
   }
 
   vector<wstring> args(argv, argv + argc);
@@ -55,7 +56,7 @@ int wmain(int argc, wchar_t *argv[]) {
   )), [&PrintErrMessage](HANDLE *instance) {
     if (*instance) {
       if (!CloseHandle(*instance)) PrintErrMessage(0L);
-      else wcout << L"[*] success" << endl;
+      else wcout << L"[*] the process is successfully closed" << endl;
     }
   });
 
@@ -63,15 +64,17 @@ int wmain(int argc, wchar_t *argv[]) {
     PrintErrMessage(0L);
     return 1;
   }
+  wcout << L"[*] process (" << pid << L") is successfully opened" << endl;
 
   PROCESS_BASIC_INFORMATION pbi = {0}; // getting PEB address
   NTSTATUS nts = NtQueryInformationProcess(
     *ps, ProcessBasicInformation, &pbi, sizeof(pbi), nullptr
   );
-  if (!NT_SUSCCES(nts)) {
+  if (!NT_SUCCESS(nts)) {
     PrintErrMessage(nts);
     return 1;
   }
+  wcout << L"[*] PEB located at " << pbi.PebBaseAddress << endl;
 
   ULONG count = 0; // getting number of heaps
   if (!ReadProcessMemory(
@@ -81,8 +84,7 @@ int wmain(int argc, wchar_t *argv[]) {
     PrintErrMessage(0L);
     return 1;
   }
-  wcout << L"[*] process ("
-        << argv[1] << L") => " << count << L" heaps" << endl;
+  wcout << L"[*] the process has " << count << L" heaps" << endl;
 
   PVOID address = nullptr; // getting address of heaps
   if (!ReadProcessMemory(
@@ -92,14 +94,47 @@ int wmain(int argc, wchar_t *argv[]) {
     PrintErrMessage(0L);
     return 1;
   }
+  wcout << L"[*] heaps located at " << address << endl;
 
   vector<PVOID> heaps(sizeof(PVOID) * count);
   if (!ReadProcessMemory(*ps, address, &heaps[0], heaps.size(), nullptr)) {
     PrintErrMessage(0L);
     return 1;
   }
+  wcout << L"No  Heap             Flags    Rsrv(Kb) Commit(Kb) Segments FastHeap" << endl;
+  wcout << L"--- -----            ------   -------- ---------- -------- --------" << endl;
   for (ULONG i = 0; i < count; i++) {
-    wcout << i + 1 << L": " << heaps[i] << endl;
+    ULONG flags = 0; // getting heap flags
+    if (!ReadProcessMemory(
+      *ps, static_cast<PCHAR>(heaps[i]) + HeapFlags,
+      &flags, sizeof(ULONG), nullptr
+    )) continue;
+
+    HEAP_COUNTERS hc = {0};
+    if (!ReadProcessMemory(
+      *ps, static_cast<PCHAR>(heaps[i]) + HeapCounters,
+      &hc, sizeof(HEAP_COUNTERS), nullptr
+    )) continue;
+
+    BYTE type = 0; // getting heap type
+    if (!ReadProcessMemory(
+      *ps, static_cast<PCHAR>(heaps[i]) + HeapFrontEndHeapType,
+      &type, sizeof(BYTE), nullptr
+    )) continue;
+
+    wcout << setw(2) << setfill(L' ') << i + 1 << L": " // heap number
+          << heaps[i] // heap address
+          << L" " << hex << setw(8) << setfill(L'0') << flags // flags
+          << dec // all other values are decimal
+          << L" " << setw(8) << setfill(L' ')
+          << hc.TotalMemoryReserved / 1024 // reserved
+          << L" " << setw(10) << setfill(L' ')
+          << hc.TotalMemoryCommitted / 1024 // commit
+          << L" " << setw(8) << setfill(L' ')
+          << hc.TotalSegments // segments
+          << L" " << setw(8) << setfill(L' ')
+          << (2 == type ? L"LFH" : L"") // heap type
+          << endl; // end of line
   }
 
   return 0;
--- cpp/psheap/peb/psheap.hpp
+++ cpp/psheap/peb/psheap.hpp
@@ -6,52 +6,70 @@
 
 #include <windows.h>
 
-typedef LONG KPRIORITY;
-typedef LONG NTSTATUS;
-
-// PEB (Win10)
-#ifdef _WIN64
-  #define PebLength        (static_cast<SIZE_T>(0x7C8))
-  #define PebNumberOfHeaps (static_cast<WORD>(0x0E8))
-  #define PebProcessHeaps  (static_cast<WORD>(0x0F0))
-#elif _WIN32
-  #define PebLength        (static_cast<SIZE_T>(0x480))
-  #define PebNumberOfHeaps (static_cast<WORD>(0x088))
-  #define PebProcessHeaps  (static_cast<WORD>(0x090))
+#ifdef _M_X64 // WIN10
+  // PEB offsets
+  #define PebLength            (static_cast<SIZE_T>(0x7C8)) // sizeof(PEB)
+  #define PebNumberOfHeaps     (static_cast<USHORT>(0x0E8)) // ULONG  NumberOfHeaps;
+  #define PebProcessHeaps      (static_cast<USHORT>(0x0F0)) // PHEAP  ProcessHeaps;
+  // HEAP offsets
+  #define HeapLength           (static_cast<SIZE_T>(0x2C0)) // sizeof(HEAP)
+  #define HeapFlags            (static_cast<USHORT>(0x070)) // ULONG  Flags;
+  #define HeapFrontEndHeapType (static_cast<USHORT>(0x1A2)) // BYTE
+  #define HeapCounters         (static_cast<USHORT>(0x238)) // HEAP_COUNTERS
+#else
+  // PEB offsets
+  #define PebLength            (static_cast<SIZE_T>(0x480))
+  #define PebNumberOfHeaps     (static_cast<USHORT>(0x088))
+  #define PebProcessHeaps      (static_cast<USHORT>(0x090))
+  // HEAP offsets
+  #define HeapLength           (static_cast<SIZE_T>(0x258))
+  #define HeapFlags            (static_cast<USHORT>(0x040))
+  #define HeapFrontEndHeapType (static_cast<USHORT>(0x0EA))
+  #define HeapCounters         (static_cast<USHORT>(0x1F4))
 #endif
 
+typedef LONG KPRIORITY;
+typedef LONG NTSTATUS;
 #define ProcessBasicInformation 0
-#define NT_SUSCCES(Status) ((static_cast<NTSTATUS>(Status)) >= 0L)
-/*
-#pragma comment (lib, "ntdll.lib")
-
-NTSYSCALLAPI
-NTSTATUS
-NTAPI NtQueryInformationProcess(
-   _In_ HANDLE ProcessHandle,
-   _In_ PROCESSINFOCLASS ProcessInfoClass,
-   _Out_writes_bytes_(ProcessInformationLength) PVOID ProcessInformation,
-   _In_ ULONG ProcessInformationLength,
-   _Out_opt_ PULONG ReturnLength
-);
-
-NTSYSAPI
-ULONG
-NTAPI RtlNtStatusToDosError(
-   _In_ NTSTATUS Status
-);
-*/
+#define NT_SUCCESS(Status) ((static_cast<NTSTATUS>(Status)) >= 0L)
 #define AddrToFunc(T) (reinterpret_cast<T>(GetProcAddress(GetModuleHandle(L"ntdll.dll"), (&((#T)[1])))))
 typedef NTSTATUS (__stdcall *pNtQueryInformationProcess)(HANDLE, ULONG, PVOID, ULONG, PULONG);
 typedef ULONG    (__stdcall *pRtlNtStatusToDosError)(NTSTATUS);
+
 pNtQueryInformationProcess NtQueryInformationProcess;
-pRtlNtStatusToDosError RtlNtStatusToDosError;
+pRtlNtStatusToDosError     RtlNtStatusToDosError;
 
 typedef struct _PROCESS_BASIC_INFORMATION {
    NTSTATUS  ExitCode;
-   PVOID     PebBaseAddress; // PPEB
-   ULONG_PTR AffinityNask;
+   PVOID     PebBaseAddress;
+   ULONG_PTR AffinityMask;
    KPRIORITY BasePriority;
    HANDLE    UniqueProcessId;
    HANDLE    InheritedFromUniqueProcessId;
 } PROCESS_BASIC_INFORMATION, *PPROCESS_BASIC_INFORMATION;
+
+typedef struct _HEAP_COUNTERS {
+   UINT_PTR TotalMemoryReserved;
+   UINT_PTR TotalMemoryCommitted;
+   UINT_PTR TotalMemoryLargeUCR;
+   UINT_PTR TotalSizeInVirtualBlocks;
+   ULONG    TotalSegments;
+   ULONG    TotalUCRs;
+   ULONG    CommittOps;
+   ULONG    DeCommitOps;
+   ULONG    LockAcquires;
+   ULONG    LockCollisions;
+   ULONG    CommitRate;
+   ULONG    DecommittRate;
+   ULONG    CommitFailures;
+   ULONG    InBlockCommitFailures;
+   ULONG    PollIntervalCounter;
+   ULONG    DecommitsSinceLastCheck;
+   ULONG    HeapPollInterval;
+   ULONG    AllocAndFreeOps;
+   ULONG    AllocationIndicesActive;
+   ULONG    InBlockDeccommits;
+   UINT_PTR InBlockDeccomitSize;
+   UINT_PTR HighWatermarkSize;
+   UINT_PTR LastPolledSize;
+} HEAP_COUNTERS, *PHEAP_COUNTERS;
