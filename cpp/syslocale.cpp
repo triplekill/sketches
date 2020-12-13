6adde1c22ec1797b0d5124adc5fe687270123e5c
gregzakh <grishanz@yandex.ru>
2020-12-14 00:03:26 +0500

    M cpp/syslocale.cpp

--- cpp/syslocale.cpp
+++ cpp/syslocale.cpp
@@ -9,7 +9,7 @@
 //////////////////////////////////////////////////////////////////////////////////////
 typedef LONG NTSTATUS;
 
-#define NT_SUCCESS(Status) ((static_cast<NTSTATUS>(Status)) >= 0L)
+#define NT_SUCCESS(Status) (static_cast<NTSTATUS>(Status) >= 0L)
 #define AddrToFunc(T) (reinterpret_cast<T>( \
                          GetProcAddress(GetModuleHandle(L"ntdll.dll"), (&((#T)[1])))))
 //////////////////////////////////////////////////////////////////////////////////////
@@ -60,13 +60,43 @@ BOOLEAN LocateSignatures(void) {
   return TRUE;
 }
 
-int wmain(void) {
-  using std::endl;
-  using std::hex;
-  using std::locale;
-  using std::vector;
-  using std::wcout;
+LCID GetCurrentLocale(void) {
+#ifdef __clang__
+  LCID *locale{};
+  __asm {
+  #ifdef _M_X64
+    mov rax, qword ptr gs:[0x30] ; PTEB
+    mov rax, [rax + 0x108]       ; PTEB->CurrentLocale
+    mov locale, rax
+  #else
+    mov eax, dword ptr fs:[0x18]
+    mov eax, [eax + 0x0C4]
+    mov locale, eax
+  #endif
+  }
+  return (LCID)locale;
+#elif _MSC_VER
+  #if _M_X64
+    // mov rax, dword ptr gs:[0x30]
+    // mov eax, dword ptr [rax+0x108]
+    // ret
+    return *(LCID *)(__readgsqword(0x30) + 0x108);
+  #else
+    // mov eax, dword ptr fs:[0x18]
+    // mov eax, dword ptr [eax+0x0C4]
+    // ret
+    return *(LCID *)(__readfsdword(0x18) + 0x0C4);
+  #endif
+#else
+  LCID locale{};
+  // call but not check NTSTATUS (this's draft, dude!)
+  NtQueryDefaultLocale(FALSE, &locale);
+  return locale;
+#endif
+}
 
+int wmain(void) {
+  using namespace std;
   locale::global(locale(""));
   auto getlasterror = [](NTSTATUS nts) {
     vector<wchar_t> msg(0x100);
@@ -84,15 +114,9 @@ int wmain(void) {
     return 1;
   }
 
-  LCID lcid{};
-  NTSTATUS nts = NtQueryDefaultLocale(FALSE, &lcid);
-  if (!NT_SUCCESS(nts)) {
-    getlasterror(nts);
-    return 1;
-  }
-
+  LCID lcid = GetCurrentLocale();
   UNICODE_STRING name;
-  nts = RtlLcidToLocaleName(lcid, &name, 0, TRUE);
+  NTSTATUS nts = RtlLcidToLocaleName(lcid, &name, 0, TRUE);
   if (!NT_SUCCESS(nts)) {
     getlasterror(nts);
     return 1;
