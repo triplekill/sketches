32708e38aa6cf5c0016422a2f82f40c37555f684
gregzakh <grishanz@yandex.ru>
2020-12-22 00:10:30 +0500

    M asm/ticks.asm

--- asm/ticks.asm
+++ asm/ticks.asm
@@ -6,17 +6,17 @@ fmt: db "%llu", 0xA, 0
 
 section .text
 main:
-  sub  rsp, 38h                   ; reserve stack
-  mov  rax, qword [abs 7FFE0320h] ; KUSER_SHARED_DATA->TickCountQuad
-  mov  qword [rsp+28h], rax
-  mov  eax, dword [abs 7FFE0004h] ; KUSER_SHARED_DATA->TickCountMultiplier
-  mov  dword [rsp+20h], eax
-  mov  eax, dword [rsp+20h]
-  imul rax, qword [rsp+28h]
-  shr  rax, 18h
-  mov  rdx, rax
-  lea  rcx, [rel fmt]
-  call printf
-  xor  eax, eax
-  add  rsp, 38h
-  ret
+    sub  rsp, 38h                   ; reserve stack
+    mov  rax, qword [abs 7FFE0320h] ; KUSER_SHARED_DATA->TickCountQuad
+    mov  qword [rsp+28h], rax
+    mov  eax, dword [abs 7FFE0004h] ; KUSER_SHARED_DATA->TickCountMultiplier
+    mov  dword [rsp+20h], eax
+    mov  eax, dword [rsp+20h]
+    imul rax, qword [rsp+28h]
+    shr  rax, 18h
+    mov  rdx, rax
+    lea  rcx, [rel fmt]
+    call printf
+    xor  eax, eax
+    add  rsp, 38h
+    ret
