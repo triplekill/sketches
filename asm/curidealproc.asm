a67cbb487502f11bd744c79e7b4125bda7271e24
gregzakh <grishanz@yandex.com>
2020-12-28 22:09:47 +0500

    M asm/curidealproc.asm

--- asm/curidealproc.asm
+++ asm/curidealproc.asm
@@ -2,7 +2,7 @@ global main
 extern printf
 
 section .bss
-struc PROCESS0R_NUMBER
+struc PROCESSOR_NUMBER
    .Group:    resw 1
    .Number:   resb 1
    .Reserved: resb 1
@@ -16,16 +16,16 @@ section .text
 main:
     push  rdi
     sub   rsp, 30h
-    lea   rax, [rsp+PROCESS0R_NUMBER]
+    lea   rax, [rsp+PROCESSOR_NUMBER]
     mov   rdi, rax
     xor   eax, eax
     mov   ecx, 4
     rep   stosb
     mov   rax, qword [gs:abs 30h]
     mov   eax, dword [rax+1744h]
-    mov   dword [rsp+PROCESS0R_NUMBER], eax
-    movzx eax, byte [rsp+PROCESS0R_NUMBER.Number]
-    movzx ecx, word [rsp+PROCESS0R_NUMBER.Group]
+    mov   dword [rsp+PROCESSOR_NUMBER], eax
+    movzx eax, byte [rsp+PROCESSOR_NUMBER.Number]
+    movzx ecx, word [rsp+PROCESSOR_NUMBER.Group]
     mov   r8d, eax
     mov   edx, ecx
     lea   rcx, [rel fmt]
