84845f83210a05ad58fd89074a1d3de58d2f337e
gregzakh <grishanz@yandex.com>
2021-03-31 00:15:05 +0500

    M pwsh/Get-ProcAddress.ps1

--- pwsh/Get-ProcAddress.ps1
+++ pwsh/Get-ProcAddress.ps1
@@ -15,14 +15,11 @@ function Get-ProcAddress {
   end {
     $mod = ($ps = Get-Process -Id $PID).Modules
     $ps.Dispose()
-    # check main executable bitness
     $jmp = [Marshal]::ReadInt32($mod[0].BaseAddress, 0x3C) + [Marshal]::SizeOf([UInt32]0)
     $jmp = switch ([BitConverter]::ToUInt16( # make sure the number is unsigned
       [BitConverter]::GetBytes([Marshal]::ReadInt32($mod[0].BaseAddress, $jmp)), 0
     )) { 0x14C {0x20, 0x78} 0x8664 {0x40, 0x88} default { throw } }
-    # helper converter
     $to_i = "ToInt$($jmp[0])"
-    # locating reuired module
     if (!($ib = $mod.Where{$_.ModuleName -match "^$Module"}.BaseAddress)) {
       throw [DllNotFoundException]::new("Cannot find $Module library.")
     }
