3e48cc68fead046765596db3892ba7603dbf54dc
gregzakh <grishanz@yandex.com>
2021-04-01 00:22:20 +0500

    M pwsh/New-Delegate.ps1

--- pwsh/New-Delegate.ps1
+++ pwsh/New-Delegate.ps1
@@ -1,17 +1,6 @@
-#requires -version 6.1
 using namespace System.Reflection
 using namespace System.Linq.Expressions
 using namespace System.Runtime.InteropServices
-
-$keys, $types = ($accel = [PSObject].Assembly.GetType(
-  'System.Management.Automation.TypeAccelerators'
-))::Get.Keys, @{
-  buf   = [Byte[]]
-  ptr   = [IntPtr]
-  int_  = [Int32].MakeByRefType()
-  uint_ = [UInt32].MakeByRefType()
-}
-$types.Keys.ForEach{ if ($_ -notin $keys) { $accel::Add($_, $types.$_) } }
 
 function New-Delegate {
   [CmdletBinding()]
@@ -26,67 +15,64 @@ function New-Delegate {
   )
 
   begin {
-    $kernel32, $method = @{}, [Expression].Assembly.GetType(
-      'System.Linq.Expressions.Compiler.DelegateHelpers'
-    ).GetMethod('MakeNewCustomDelegate', [BindingFlags]'NonPublic, Static')
-    # GetDelegateForFunctionPointer<T>(IntPtr ptr)
-    $fn = [Marshal].GetMethod('GetDelegateForFunctionPointer', ([IntPtr]))
-    [Array]::Find(( # GetModuleHandle and GetProcAddress
-      Add-Type -AssemblyName Microsoft.Win32.SystemEvents -PassThru
-    ), [Predicate[Type]]{$args[0].Name -eq 'kernel32'}
-    ).GetMethods([BindingFlags]'NonPublic, Static, Public').Where{
-      $_.Name -cmatch '\AGet(M|P)'
-    }.ForEach{ $kernel32[$_.Name] = $_ }
-
-    if ((
-      $mod = $kernel32.GetModuleHandle.Invoke($null, @($Module))
-    ) -eq [IntPtr]::Zero) {
-      throw [DllNotFoundException]::new('Can not find the library.')
+    $mod = ($ps = Get-Process -Id $PID).Modules
+    $ps.Dispose()
+    $jmp = [Marshal]::ReadInt32($mod[0].BaseAddress, 0x3C) + [Marshal]::SizeOf([UInt32]0)
+    $jmp = switch([BitConverter]::ToUInt16( # make sure the number is unsigned
+      [BitConverter]::GetBytes([Marshal]::ReadInt32($mod[0].BaseAddress, $jmp)), 0
+    )) { 0x14C {0x20, 0x78} 0x8664 {0x40, 0x88} default { throw } }
+    $to_i = "ToInt$($jmp[0])"
+    if (!($ib = $mod.Where{$_.ModuleName -match "^$Module"}.BaseAddress)) {
+      throw [DllNotFoundException]::new("Cannot find $Module library.")
+    }
+    $tmp = $ib.$to_i()
+    $va = [Marshal]::ReadInt32([IntPtr]([Marshal]::ReadInt32($ib, 0x3C) + $tmp + $jmp[1]))
+    $ed = @{bs = 0x10; nf = 0x14; nn = 0x18; af = 0x1C; an = 0x20; ao = 0x24}
+    $ed.Keys.ForEach{ # key fields of IMAGE_EXPORT_DIRECTORY
+      $val = [Marshal]::ReadInt32($ib, $va + $ed.$_)
+      Set-Variable -Name $_ -Value ($_.StartsWith('a') ? $tmp + $val : $val) -Scope Script
+    }
+    $funcs, $names = @{}, @{}
+    (0..($nf - 1)).ForEach{
+      $funcs[$bs + $_] = [IntPtr]($tmp + [Marshal]::ReadInt32([IntPtr]($af + $_ * 4)))
+    }
+    (0..($nn - 1)).ForEach{
+      $names[($bs + [Marshal]::ReadInt16([IntPtr]($ao + $_ * 2)))] = [Marshal]::PtrToStringAnsi(
+        [IntPtr]($tmp + [Marshal]::ReadInt32([IntPtr]($an + $_ * 4)))
+      )
+    }
+    $exports = $funcs.Keys.ForEach{
+      [PSCustomObject]@{
+        Ordinal = $_
+        Address = $funcs[$_]
+        Name    = $names[$_]
+      }
     }
   }
   process {}
   end {
     $funcs = @{}
-    $proto = $Signature.Ast.FindAll({$args[0].StringConstantType}, $true).ToArray()
+    for ($i, $m, $fn, $p = 0, ([Expression].Assembly.GetType(
+        'System.Linq.Expressions.Compiler.DelegateHelpers'
+      ).GetMethod('MakeNewCustomDelegate', [BindingFlags]'NonPublic, Static')
+      ), [Marshal].GetMethod('GetDelegateForFunctionPointer', ([IntPtr])),
+      $Signature.Ast.FindAll({$args[0].CommandElements}, $true).ToArray();
+      $i -lt $p.Length; $i++
+    ) {
+      $fnret, $fname = ($def = $p[$i].CommandElements).Value
+      $fnsig, $fnarg = $exports.Where{$_.Name -ceq $fname}.Address, $def.Pipeline.Extent.Text
 
-    for ($i = 0; $i -lt $proto.Length; $i += 2) {
-      $block = $proto[$i..($i + 1)] # return type and function name with parameters
-      if (!(($returntype = $block[0].Extent.Text) -as [Type])) {
-        throw [InvalidCastException]::new('Unknown data type.')
-      }
-      $fnname = $block[1].Extent.Text
+      [Object[]]$fnarg = [String]::IsNullOrEmpty($fnarg) ? $fnret : (
+        ($fnarg -replace '\[|\]' -split ',\s+?').ForEach{
+          $_.StartsWith('_') ? (Get-Variable $_.Remove(0, 1) -ValueOnly) : $_
+        } + $fnret
+      )
 
-      if (( # function signature (address)
-        $fnaddr = $kernel32.GetProcAddress.Invoke($null, @($mod, $fnname))
-      ) -eq [IntPtr]::Zero) {
-        throw [InvalidOperationException]::new('Can not find function signature.')
-      }
-
-      $fnargs = $block[1].Parent.CommandElements[-1].Pipeline.Extent.Text
-      [Object[]]$fnargs = (( # unparameterized function or not
-        ($fnargs -replace'\[|\]' -split ',\s+?') + $returntype
-      ), $returntype)[[String]::IsNullOrEmpty($fnargs)]
-
-      $funcs[$fnname] = $fn.MakeGenericMethod(
-        [Delegate]::CreateDelegate([Func[[Type[]], Type]], $method).Invoke($fnargs)
-      ).Invoke([Marshal], $fnaddr)
+      $funcs[$fname] = $fn.MakeGenericMethod(
+        [Delegate]::CreateDelegate([Func[[Type[]], Type]], $m).Invoke($fnarg)
+      ).Invoke([Marshal], $fnsig)
     }
-    $funcs
+
+    Set-Variable -Name $Module -Value $funcs -Scope Script
   }
 }
-
-<#
-# Usage example
-
-$ntdll = New-Delegate ntdll -Signature {
-  int NtQueryTimerResolution([uint_, uint_, uint_])
-  int RtlNtStatusToDosError([int])
-}
-
-$mx, $mn, $cr = [UInt32[]](,0 * 3)
-if (($nts = $ntdll.NtQueryTimerResolution.Invoke([ref]$mx, [ref]$mn, [ref]$cr)) -ne 0) {
-  [ComponentModel.Win32Exception]::new($ntdll.RtlNtStatusToDosError.Invoke($nts)).Message
-  break
-}
-($mx, $mn, $cr).ForEach{ '{0:F3} ms' -f ($_ / 10000) }
-#>
