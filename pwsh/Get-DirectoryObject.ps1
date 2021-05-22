4df82ba0841b3cef98b2e1ad239453e33d766aa3
gregzakh <grishanz@yandex.com>
2021-05-23 01:07:42 +0500

    M pwsh/Get-DirectoryObject.ps1

--- pwsh/Get-DirectoryObject.ps1
+++ pwsh/Get-DirectoryObject.ps1
@@ -1,8 +1,5 @@
-#requires -version 7
-
 using namespace System.Reflection
 using namespace System.ComponentModel
-using namespace System.Reflection.Emit
 using namespace System.Linq.Expressions
 using namespace Microsoft.Win32.SafeHandles
 using namespace System.Runtime.InteropServices
@@ -20,18 +17,37 @@ function New-Delegate {
   )
 
   begin {
-    $kernel32 = @{}
-    [Array]::Find((
-      Add-Type -AssemblyName Microsoft.Win32.SystemEvents -PassThru
-    ), [Predicate[Type]]{$args[0].Name -eq 'kernel32'}).GetMethods(
-      [BindingFlags]'NonPublic, Static, Public'
-    ).Where{$_.Name -cmatch '\AGet(P|M)'}.ForEach{$kernel32[$_.Name] = $_}
-
-    if (($mod = $kernel32.GetModuleHandle.Invoke($null, @($Module))) -eq [IntPtr]::Zero) {
-      throw [DllNotFoundException]::new("Cannot find $Module library.")
+    if (!($exp = $ExecutionContext.SessionState.PSVariable.Get("__$Module").Value)) {
+      $mod = ($ps = Get-Process -Id $PID).Modules.Where{$_.ModuleName -match "^$Module"}.BaseAddress
+      $ps.Dispose() && ($mod ? $($jmp = ( # checking that DLL is loaded, otherwise throw an exception
+        $mov = [Marshal]::ReadInt32($mod, 0x3C)) + [Marshal]::SizeOf([UInt32]0) # e_lfanew
+      ) : $(throw [DllNotFoundException]::new("It seems that $Module is not loaded.")))
+      $jmp = switch ([BitConverter]::ToUInt16([BitConverter]::GetBytes([Marshal]::ReadInt16($mod, $jmp)), 0)) {
+        0x014C { 0x20, 0x78, 0x7C } 0x8664 { 0x40, 0x88, 0x8C } default { throw [SystemException]::new() }
+      } # getting Export Directory values and key fields of the IMAGE_EXPORT_DIRECTORY structure
+      $tmp, $fun = $mod."ToInt$($jmp[0])"(), @{}
+      $va, $sz = $jmp[1,2].ForEach{[Marshal]::ReadInt32($mod, $mov + $_)}
+      ($ed = @{bs = 0x10; nf = 0x14; nn = 0x18; af = 0x1C; an = 0x20; ao = 0x24}).Keys.ForEach{
+        $val = [Marshal]::ReadInt32($mod, $va + $ed.$_)
+        Set-Variable -Name $_ -Value ($_.StartsWith('a') ? $tmp + $val : $val) -Scope Script
+      } # well, it's time to get the export table itself
+      function Assert-Forwarder([UInt32]$fa) { end { ($va -le $fa) -and ($fa -lt ($va + $sz))} }
+      (0..($nf - 1)).ForEach{
+        $fun[$bs + $_] = (Assert-Forwarder ($fa = [Marshal]::ReadInt32([IntPtr]($af + $_ * 4)))) ? @{
+          Address = ''; Forward = [Marshal]::PtrToStringAnsi([IntPtr]($tmp + $fa))
+        } : @{Address = [IntPtr]($tmp + $fa); Forward = ''}
+      }
+      Set-Variable -Name "__$Module" -Value ($exp = (0..($nn - 1)).ForEach{
+        [PSCustomObject]@{
+          Ordinal = ($ord = $bs + [Marshal]::ReadInt16([IntPtr]($ao + $_ * 2)))
+          Address = $fun[$ord].Address
+          Name = [Marshal]::PtrToStringAnsi([IntPtr]($tmp + [Marshal]::ReadInt32([IntPtr]($an + $_ * 4))))
+          Forward = $fun[$ord].Forward
+        }
+      }) -Option ReadOnly -Scope Global -Visibility Private
     }
   }
-  process {}
+  # process {} # ignore this block
   end {
     $funcs = @{}
     for ($i, $m, $fn, $p = 0, ([Expression].Assembly.GetType(
@@ -40,163 +56,25 @@ function New-Delegate {
       ), [Marshal].GetMethod('GetDelegateForFunctionPointer', ([IntPtr])),
       $Signature.Ast.FindAll({$args[0].CommandElements}, $true).ToArray();
       $i -lt $p.Length; $i++
-    ) {
+    ) { # everything is ready to create a set of delegates... let's do it!
       $fnret, $fname = ($def = $p[$i].CommandElements).Value
+      $fnsig, $fnarg = $exp.Where{$_.Name -ceq $fname}.Address, $def.Pipeline.Extent.Text
 
-      if (($fnsig = $kernel32.GetProcAddress.Invoke(
-        $null, @($mod, $fname)
-      )) -eq [IntPtr]::Zero) {
-        throw [InvalidOperationException]::new("Cannot find $fname signature.")
-      }
+      if (!$fnsig) { throw [InvalidOperationException]::new("Cannot find $fname signature. Are you correct?") }
 
-      $fnargs = $def.Pipeline.Extent.Text
-      [Object[]]$fnargs = [String]::IsNullOrEmpty($fnargs) ? $fnret : (
-                                          $fnargs -replace '\[|\]' -split ',\s+?') + $fnret
+      [Object[]]$fnarg = [String]::IsNullOrEmpty($fnarg) ? $fnret : (
+        ($fnarg -replace '\[|\]' -split ',\s+').ForEach{
+          $_.StartsWith('_') ? (Get-Variable $_.Remove(0, 1) -ValueOnly) : $_
+        } + $fnret
+      )
+
       $funcs[$fname] = $fn.MakeGenericMethod(
-        [Delegate]::CreateDelegate([Func[[Type[]], Type]], $m).Invoke($fnargs)
+        [Delegate]::CreateDelegate([Func[[Type[]], Type]], $m).Invoke($fnarg)
       ).Invoke([Marshal], $fnsig)
     }
-    Set-Variable -Name $Module -Value $funcs -Scope Script -Force
+    Set-Variable -Name $Module -Value $funcs -Scope Script
   }
 }
-
-function Get-DynBuilder {
-  end {
-    if (!($pmb = $ExecutionContext.SessionState.PSVariable.Get('PwshDynBuilder').Value)) {
-      Set-Variable -Name PwshDynBuilder -Value ($pmb =
-        ([AssemblyBuilder]::DefineDynamicAssembly(
-          ([AssemblyName]::new('PwshDynBuilder')), 'Run'
-        )).DefineDynamicModule('PwshDynBuilder', $false)
-      ) -Option Constant -Scope Global -Visibility Private
-      $pmb
-    }
-    else {$pmb}
-  }
-}
-
-function New-Structure {
-  [CmdletBinding()]
-  param(
-    [Parameter(Mandatory, Position=0)]
-    [ValidateNotNullOrEmpty()]
-    [String]$Name,
-
-    [Parameter(Mandatory, Position=1)]
-    [ValidateScript({![String]::IsNullOrEmpty($_)})]
-    [ScriptBlock]$Definition,
-
-    [Parameter()]
-    [ValidateSet(
-      'Unspecified', 'Size1', 'Size2', 'Size4', 'Size8', 'Size16', 'Size32', 'Size64', 'Size128'
-    )]
-    [ValidateNotNullOrEmpty()]
-    [PackingSize]$PackingSize = 'Unspecified',
-
-    [Parameter()]
-    [ValidateSet('Ansi', 'Auto', 'Unicode')]
-    [ValidateNotNullOrEmpty()]
-    [CharSet]$CharSet = 'Ansi',
-
-    [Parameter()]
-    [Switch]$Explicit
-  )
-
-  begin {
-    [TypeAttributes]$attr = 'BeforeFieldInit, Class, Public, Sealed'
-    $attr = $attr -bor ($Explicit.IsPresent ? 'Explicit' : 'Sequential') -bor "$($CharSet)Class"
-  }
-  process {}
-  end {
-    if (!($pmb = Get-DynBuilder).GetType($Name)) {
-      $type = $pmb.DefineType($Name, $attr, [ValueType], $PackingSize)
-      $ctor = [MarshalAsAttribute].GetConstructor(
-        [BindingFlags]'Instance, Public', $null, [Type[]]([UnmanagedType]), $null
-      )
-      $sc = [MarshalAsAttribute].GetField('SizeConst')
-
-      $Definition.Ast.FindAll({$args[0].CommandElements}, $true).ToArray().ForEach{
-        $ftype, $fdesc = $_.CommandElements.Value
-        $ftype = $pmb.GetType($ftype) ?? [Type]$ftype
-        $fdesc = @(($fdesc -split '\s+?').Where{$_}) # field, param ...
-        switch ($fdesc.Length) {
-          1 {[void]$type.DefineField($fdesc[0], $ftype, 'Public')}
-          2 {
-            [void]($Explicit.IsPresent ? $type.DefineField($fdesc[0], $ftype, 'Public'
-            ).SetOffset([Int32]$fdesc[1]) : $type.DefineField(
-              $fdesc[0], $ftype, 'Public, HasFieldMarshal'
-            ).SetCustomAttribute(
-              [CustomAttributeBuilder]::new($ctor, [Object]([UnmanagedType]$fdesc[1]))
-            ))
-          }
-          3 {
-            [void]$type.DefineField($fdesc[0], $ftype, 'Public, HasFieldMarshal'
-            ).SetCustomAttribute(
-              [CustomAttributeBuilder]::new($ctor, [UnmanagedType]$fdesc[1], $sc, ([Int32]$fdesc[2]))
-            )
-          }
-        }
-      }
-      $il = $type.DefineMethod('GetSize', 'Public, Static', [Int32], [Type[]]@()).GetILGenerator()
-      $il.Emit([OpCodes]::ldtoken, $type)
-      $il.Emit([OpCodes]::call, [Type].GetMethod('GetTypeFromHandle'))
-      $il.Emit([OpCodes]::call, [Marshal].GetMethod('SizeOf', [Type[]]([Type])))
-      $il.Emit([OpCodes]::ret)
-      $il = $type.DefineMethod(
-        'op_Implicit', 'PrivateScope, Public, Static, HideBySig, SpecialName', $type, [Type]([IntPtr])
-      ).GetILGenerator()
-      $il.Emit([OpCodes]::ldarg_0)
-      $il.Emit([OpCodes]::ldtoken, $type)
-      $il.Emit([OpCodes]::call, [Type].GetMethod('GetTypeFromHandle'))
-      $il.Emit([OpCodes]::call, [Marshal].GetMethod('PtrToStructure', [Type[]]([IntPtr], [Type])))
-      $il.Emit([OpCodes]::unbox_any, $type)
-      $il.Emit([OpCodes]::ret)
-      [void]$type.CreateType()
-    }
-  }
-}
-
-######################################################################################
-
-New-Structure UNICODE_STRING {
-  UInt16 Length
-  UInt16 MaximumLength
-  String 'Buffer LPWstr'
-} -CharSet Unicode
-
-New-Structure OBJECT_DIRECTORY_INFORMATION {
-  UNICODE_STRING Name
-  UNICODE_STRING TypeName
-}
-
-New-Structure OBJECT_ATTRIBUTES {
-  UInt32 Length
-  IntPtr RootDirectory
-  IntPtr ObjectName
-  UInt32 Attributes
-  IntPtr SecurityDescriptor
-  IntPtr SecurityQualityOfService
-}
-
-$keys, $types = ($x = [PSObject].Assembly.GetType(
-  'System.Management.Automation.TypeAccelerators'
-))::Get.Keys, @{
-  ptr      = [IntPtr]
-  uint_    = [UInt32].MakeByRefType()
-  ustr_    = [UNICODE_STRING].MakeByRefType()
-  sfh      = [SafeFileHandle]
-  sfh_     = [SafeFileHandle].MakeByRefType()
-  objattr_ = [OBJECT_ATTRIBUTES].MakeByRefType()
-}
-$types.Keys.ForEach{if ($_ -notin $keys) {$x::Add($_, $types.$_)}}
-
-New-Delegate ntdll {
-  int NtOpenDirectoryObject([sfh_, uint, objattr_])
-  int NtQueryDirectoryObject([sfh, ptr, uint, bool, bool, uint_, uint_])
-  int RtlNtStatusToDosError([int])
-  void RtlInitUnicodeString([ustr_, ptr])
-}
-
-######################################################################################
 
 function Get-DirectoryObject {
   [CmdletBinding()]
@@ -206,58 +84,66 @@ function Get-DirectoryObject {
     [String]$Path
   )
 
+  begin {
+    $sfh,$ptr = [SafeFileHandle], [IntPtr]
+    $buf, $sfh_, $uint_ = [Byte[]], $sfh.MakeByRefType(), [UInt32].MakeByRefType()
+
+    New-Delegate ntdll {
+      int  NtOpenDirectoryObject([_sfh_, uint, _buf])
+      int  NtQueryDirectoryObject([_sfh, _ptr, uint, bool, bool, _uint_, _uint_])
+      int  RtlNtStatusToDosError([int])
+      void RtlInitUnicodeString([_buf, _buf])
+    }
+
+    $uni = [Byte[]]::new(($psz = [IntPtr]::Size) * 2) # UNICODE_STRING
+    $usz = $uni.Length # sizeof(UNICODE_STRING)
+
+    $uni.Clear()
+    $ntdll.RtlInitUnicodeString.Invoke($uni, [Text.Encoding]::Unicode.GetBytes($Path))
+  }
+  #process {}
   end {
-    $us = [UNICODE_STRING]::new()
     try {
-      $uni = [Marshal]::StringToHGlobalUni($Path)
-      $ntdll.RtlInitUnicodeString.Invoke([ref]$us, $uni)
-
-      $atr = [OBJECT_ATTRIBUTES]::new()
-      $ptr = [Marshal]::AllocHGlobal($us::GetSize())
-      [Marshal]::StructureToPtr($us, $ptr, $true)
-
-      $atr.Length = $atr::GetSize()
-      $atr.RootDirectory = [IntPtr]::Zero
-      $atr.Attributes = 0
-      $atr.ObjectName = $ptr
-      $atr.SecurityDescriptor = [IntPtr]::Zero
-      $atr.SecurityQualityOfService = [IntPtr]::Zero
+      $gch = [GCHandle]::Alloc($uni, [GCHandleType]::Pinned)
+      [Byte[]]$obj = [BitConverter]::GetBytes($psz * 6) + (
+        ,0 * (($psz -eq 8 ? 4 : 0) + $psz) # OBJECT_ATTRIBUTES initialization
+      ) + [BitConverter]::GetBytes(
+        $gch.AddrOfPinnedObject()."ToInt$($psz * 8)"()
+      ) + (,0 * ($psz * 3))
 
       $sfh = [SafeFileHandle]::new([IntPtr]::Zero, $true)
-      if (($nts = $ntdll.NtOpenDirectoryObject.Invoke([ref]$sfh, 0x1, [ref]$atr)) -ne 0) {
-        throw [InvalidOperationException]::new([Win32Exception]::new(
-          $ntdll.RtlNtStatusToDosError.Invoke($nts)
-        ).Message)
+      if (($nts = $ntdll.NtOpenDirectoryObject.Invoke([ref]$sfh, 0x01, $obj)) -ne 0) {
+        throw [InvalidOperationException]::new(
+          [Win32Exception]::new($ntdll.RtlNtStatusToDosError.Invoke($nts)).Message
+        )
       }
 
-      $odi = [Marshal]::AllocHGlobal((
-        $bsz = [OBJECT_DIRECTORY_INFORMATION]::GetSize() * [IntPtr]::Size
-      ))
+      $odi = [Marshal]::AllocHGLobal(($bsz = $usz * 2 * $psz))
       $items, $bytes = [UInt32[]](,0 * 2)
-      while (($nts = $ntdll.NtQueryDirectoryObject.Invoke(
+      while ($ntdll.NtQueryDirectoryObject.Invoke(
         $sfh, $odi, $bsz, $false, $true, [ref]$items, [ref]$bytes
-      )) -eq 0x105) {
-        $odi = [Marshal]::ReAllocHGlobal($odi, [IntPtr]($bsz += $bytes))
-      }
+      ) -eq 0x105) { $odi = [Marshal]::ReAllocHGlobal($odi, [IntPtr]($bsz += $bytes)) }
 
-      $tmp = $odi."ToInt$([IntPtr]::Size * 8)"()
+      $tmp = $odi."ToInt$($psz * 8)"()
       (0..($items - 1)).ForEach{
-        $itm = [IntPtr]$tmp -as [OBJECT_DIRECTORY_INFORMATION]
-        [PSCustomObject]@{
-          Name = $itm.Name.Buffer
-          Type = $itm.TypeName.Buffer
+        $pair = (0..1).ForEach{
+          $sz = [Marshal]::ReadInt16([IntPtr]$tmp) / 2
+          [Marshal]::PtrToStringUni([Marshal]::ReadIntPtr([IntPtr]$tmp, $psz), $sz)
+          $tmp += $usz
         }
-        $tmp += [OBJECT_DIRECTORY_INFORMATION]::GetSize()
+        [PSCustomObject]@{
+          Name = $pair[0]
+          Type = $pair[1]
+        }
       }
     }
-    catch {Write-Verbose $_}
+    catch { Write-Verbose $_ }
     finally {
-      if ($odi) {[Marshal]::FreeHGlobal($odi)}
-      if ($sfh) {$sfh.Dispose()}
-      if ($ptr) {[Marshal]::FreeHGlobal($ptr)}
-      if ($uni) {[Marshal]::FreeHGlobal($uni)}
+      if ($odi) { [Marshal]::FreeHGlobal($odi) }
+      if ($sfh) { $sfh.Dispose() }
+      if ($gch) { $gch.Free() }
     }
   }
 }
 
-#Get-DirectoryObject '\KnownDlls'
+# Get-DirectoryObject '\'
