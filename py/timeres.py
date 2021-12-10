acbc573b9e67228dac21a94b597d89e2ea5cd755
gregzakh <grishanz@yandex.com>
2021-12-11 00:51:59 +0500

    M py/timeres.py

--- py/timeres.py
+++ py/timeres.py
@@ -12,21 +12,22 @@ if __name__ == '__main__':
       libc = CDLL('libc.so.6')
       timespec = pack('ql', 1, 2)
 
-      if 0 != libc.clock_getres(5, timespec):
+      if 0 != (err := libc.clock_getres(5, timespec)):
          libc.perror('clock_getres')
-         exit(-1)
-      print('Current timer resolution: %.3f ms' % (unpack('ql', timespec)[1] / 1000000))
+         exit(err)
+      print('Current timer resolution: %.3f ms' % (unpack('ql', timespec)[1] / 1e6))
    elif 'Windows' == os:
       from ctypes import byref, create_unicode_buffer, c_bool, c_ulong, windll
 
       tm, inc, dis = c_ulong(), c_ulong(), c_bool()
       if not windll.kernel32.GetSystemTimeAdjustment(byref(tm), byref(inc), byref(dis)):
-         err = windll.kernel32.GetLastError()
-         msg = create_unicode_buffer(0x100)
+         err, msg = windll.kernel32.GetLastError(), create_unicode_buffer(0x100)
          print('Unknown error has been occured.'
             if not windll.kernel32.FormatMessageW(
-              0x12FF, None, err, 1024, msg, len(msg), None
+               0x12FF, None, err, 0x400, msg, len(msg), None
             ) else msg.value
          )
          exit(err)
-      print('Current timer resolution: %.3f ms' % (tm.value / 10000))
+      print('Current timer resolution: %.3f ms' % (tm.value / 1e4))
+   else:
+      print('Unsupported OS version.')
